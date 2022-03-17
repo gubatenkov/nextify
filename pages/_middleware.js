9da0f22f1d4f6b5eadcc65216eb04e229d6ef244
s9379992g <s9379992g@gmail.com>
2022-03-17 23:50:41 +0200

    refactored _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,26 +1,22 @@
 import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 
-const checkAuthTokenMiddleware = async (req) => {
-  const secret = process.env.JWT_SECRET
-  // if token exist in request, user is authenticated
-  const token = await getToken({ req, secret })
+export async function middleware(req) {
+  // intercept and try to get token from req
+  const token = await getToken({ req, secret: process.env.JWT_SECRET })
+  // define path request comes from
   const { pathname } = req.nextUrl
 
   // allow requests if the following true
   // 1) User want to log in
   // 2) User already logged in / has token
-
   if (token || pathname.includes('/api/auth')) {
     return NextResponse.next()
   }
 
   // otherwise redirect user to default page if NO token AND...
   // ... user want to access protected route
-
   if (!token && pathname !== '/login') {
-    return NextResponse.redirect('/login')
+    return NextResponse.redirect(new URL('/login', req.nextUrl))
   }
 }
-
-export default checkAuthTokenMiddleware
